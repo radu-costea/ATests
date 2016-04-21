6ea7a3b0315b5229cc09b509cafc8e29e0af8ca9
Radu Costea <c_rcostea@groupon.com>
2016-04-21 15:55:28 +0300

    Keyboard avoiding

---
+++ ATests/ATests/UI/CustomViews/KeyboardPlaceholderView.swift
@@ -0,0 +1,21 @@
+//
+//  KeyboardPlaceholderView.swift
+//  ATests
+//
+//  Created by Radu Costea on 19/04/16.
+//  Copyright © 2016 Radu Costea. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardPlaceholderView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
